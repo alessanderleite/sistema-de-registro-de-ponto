a01634f18c1e8c2965f35790c2dec4dd951d6feb
Alessander <alessander.logistica@gmail.com>
2020-04-18 20:04:06 -0300

    create the dto classes

---
+++ src/main/java/br/com/alessanderleite/app/dtos/CadastroPFDto.java
@@ -0,0 +1,120 @@
+package br.com.alessanderleite.app.dtos;
+
+import java.util.Optional;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class CadastroPFDto {
+
+	private Long id;
+	
+	@NotEmpty(message = "Nome naõ pode ser vazio.")
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+	private String nome;
+	
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres")
+	@Email(message = "Email inválido.")
+	private String email;
+	
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	private String senha;
+	
+	@NotEmpty(message = "CPF não pode ser vazio.")
+	@CPF(message = "CPF inválido")
+	private String cpf;
+	
+	private Optional<String> valorHora = Optional.empty();
+	private Optional<String> qtdHorasTrabalhoDia = Optional.empty();
+	private Optional<String> qtdHorasAlmoco = Optional.empty();
+	
+	@NotEmpty(message = "CNPJ não pde ser vazio.")
+	@CNPJ(message = "CNPJ inválido.")
+	private String cnpj;
+	
+	public CadastroPFDto() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Optional<String> getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(Optional<String> valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public Optional<String> getQtdHorasTrabalhoDia() {
+		return qtdHorasTrabalhoDia;
+	}
+
+	public void setQtdHorasTrabalhoDia(Optional<String> qtdHorasTrabalhoDia) {
+		this.qtdHorasTrabalhoDia = qtdHorasTrabalhoDia;
+	}
+
+	public Optional<String> getQtdHorasAlmoco() {
+		return qtdHorasAlmoco;
+	}
+
+	public void setQtdHorasAlmoco(Optional<String> qtdHorasAlmoco) {
+		this.qtdHorasAlmoco = qtdHorasAlmoco;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public String toString() {
+		return "CadastroPFDto [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", valorHora=" + valorHora + ", qtdHorasTrabalhoDia=" + qtdHorasTrabalhoDia + ", qtdHorasAlmoco="
+				+ qtdHorasAlmoco + ", cnpj=" + cnpj + "]";
+	}
+}
---
+++ src/main/java/br/com/alessanderleite/app/dtos/CadastroPJDto.java
@@ -0,0 +1,100 @@
+package br.com.alessanderleite.app.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class CadastroPJDto {
+	private Long ind;
+	
+	@NotEmpty(message = "Nome não pode ser vazio.")
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracters")
+	private String nome;
+	
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+	@Email(message = "Email inválido.")
+	private String email;
+	
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	private String senha;
+
+	@NotEmpty(message = "CPF não pode ser vazio.")
+	@CPF(message = "CPF inválido")
+	private String cpf;
+	
+	@NotEmpty(message = "Razão Social não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Razão social deve conter entre 5 e 200 caracteres.")
+	private String razaoSocial;
+
+	@NotEmpty(message = "CNPJ não pode ser vazio.")
+	@CNPJ(message = "CNPJ inválido.")
+	private String cnpj;
+	
+	public CadastroPJDto() {}
+
+	public Long getInd() {
+		return ind;
+	}
+
+	public void setInd(Long ind) {
+		this.ind = ind;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public String toString() {
+		return "CadastroPJDto [ind=" + ind + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + "]";
+	}
+}
