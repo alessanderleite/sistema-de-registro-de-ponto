414d6226b71714437520c8989732c54cba148d7d
Alessander <alessander.logistica@gmail.com>
2020-04-18 17:54:17 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/PontoInteligenteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiApplication.class, args);
+	}
+
+}
