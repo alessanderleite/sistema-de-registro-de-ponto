d85bf1280bf98e8759f4a83ddb295737e958ffd9
Alessander <alessander.logistica@gmail.com>
2020-04-19 20:19:07 -0300

    create the util class named JwtTokenUtil

---
+++ src/main/java/br/com/alessanderleite/app/security/utils/JwtTokenUtil.java
@@ -0,0 +1,113 @@
+package br.com.alessanderleite.app.security.utils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import br.com.alessanderleite.app.entities.Funcionario;
+import br.com.alessanderleite.app.repository.FuncionarioRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenUtil {
+
+	static final String CLAIM_KEY_KEY_USERNAME = "sub,";
+	static final String CLAIM_KEY_ROLE = "role";
+	static final String CLAIM_KEY_AUDIENCE = "audience";
+	static final String CLAIM_KEY_CREATED = "created";
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	public String getUsernameFromToken(String token) {
+		String username;
+		try {
+			Claims claims = getClaimsFromToken(token);
+			username = claims.getSubject();
+		} catch (Exception e) {
+			username = null;
+		}
+		return username;
+	}
+	
+	private Date getExpirationDateFromToken(String token) {
+		Date expiration;
+		try {
+			Claims claims = getClaimsFromToken(token);
+			expiration = claims.getExpiration();
+		} catch (Exception e) {
+			expiration = null;
+		}
+		return expiration;
+	}
+
+	public String refreshToken(String token) {
+		String refreshToken;
+		try {
+			Claims claims = getClaimsFromToken(token);
+			claims.put(CLAIM_KEY_CREATED, new Date());
+			refreshToken = gerarToken(claims);
+		} catch (Exception e) {
+			refreshToken = null;
+		}
+		return refreshToken;
+	}
+	
+	public boolean tokenValido(String token) {
+		return !tokenExpirado(token);
+	}
+
+	public String obterToken(UserDetails userDetails) {
+		Funcionario funcionario = funcionarioRepository.findByEmail(userDetails.getUsername());
+		Map<String, Object> claims = new HashMap<String, Object>();
+		claims.put(CLAIM_KEY_KEY_USERNAME, userDetails.getUsername());
+		userDetails.getAuthorities().forEach(authority -> claims.put(CLAIM_KEY_ROLE, authority.getAuthority()));
+		claims.put(CLAIM_KEY_CREATED, new Date());
+		if (funcionario != null) {
+			claims.put("id", funcionario.getId());
+			claims.put("empresaId", funcionario.getEmpresa().getId());
+		}
+		return gerarToken(claims);
+	}
+	
+	private Claims getClaimsFromToken(String token) {
+		Claims claims;
+		try {
+			claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			claims = null;
+		}
+		return claims;
+	}
+	
+	private Date gerarDataExpiracao() {
+		return new Date(System.currentTimeMillis() + expiration * 1000);
+	}
+	
+	private boolean tokenExpirado(String token) {
+		Date dataExpiracao = this.getExpirationDateFromToken(token);
+		if (dataExpiracao == null) {
+			return false;
+		}
+		return dataExpiracao.before(new Date());
+	}
+
+	private String gerarToken(Map<String, Object> claims) {
+		return Jwts.builder().setClaims(claims).setExpiration(gerarDataExpiracao())
+				.signWith(SignatureAlgorithm.HS512, secret).compact();
+	}
+
+}
