da7255621eb1ba32376e41c5ab1a350aab7b7a0d
Alessander <alessander.logistica@gmail.com>
2020-04-18 23:19:40 -0300

    create the enum classes

---
+++ src/main/java/br/com/alessanderleite/app/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package br.com.alessanderleite.app.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO
+}
---
+++ src/main/java/br/com/alessanderleite/app/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package br.com.alessanderleite.app.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA,
+	TERMINO_TRABALHO
+}
